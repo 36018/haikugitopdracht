bf5cbed37d925efcf53c9782b1d2cd8f1708b27a
Reko <36913@ma-web.nl>
2023-10-09 09:47:13 +0200

    hello

--- haiku.js
+++ haiku.js
@@ -1,26 +1,27 @@
 
-function HaikuX(){
-     console.log("haiku by:","AUTHOR");
-     console.log("REGEL1");
-     console.log("REGEL2");
-     console.log("REGEL3");
+function Haiku1(){
+     console.log("haiku by:","Masaoaka Shiki");
+     console.log("Consider me");
+     console.log("As one who loved poetry");
+     console.log("And persimmons.");
 }
 
 
-function HaikuX(){
-     console.log("haiku by:","AUTHOR");
-     console.log("REGEL1");
-     console.log("REGEL2");
-     console.log("REGEL3");
+function Haiku2(){
+     console.log("haiku by:","Matsuo Bashō");
+     console.log("In the cicada's cry");
+     console.log("No sign can foretell");
+     console.log("How soon it must die.");
 }
 
 
-function HaikuX(){
-     console.log("haiku by:","AUTHOR");
-     console.log("REGEL1");
-     console.log("REGEL2");
-     console.log("REGEL3");
+function Haiku3(){
+     console.log("haiku by:","Yosa Buson");
+     console.log("Blowing from the west");
+     console.log("Fallen leaves gather");
+     console.log("In the east.");
 }
+
 
 Haiku1()
 Haiku2()
